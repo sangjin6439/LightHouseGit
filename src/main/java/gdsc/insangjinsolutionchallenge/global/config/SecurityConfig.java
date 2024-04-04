9909847eb28e7bf00f96e5be15f191a01fdc5267
sangjin <insangjin6439@gmail.com>
2024-04-04 19:29:07 +0900

    웹 주소 변경

--- src/main/java/gdsc/insangjinsolutionchallenge/global/config/SecurityConfig.java
+++ src/main/java/gdsc/insangjinsolutionchallenge/global/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/subscribe/**","/everlearning/**","https://light-house-web-pt.vercel.app","https://light-house-web-pt.vercel.app/posts/find/list/all","/posts/find/**","/","/auth/**","/ExtraLogin/**","/swagger-ui.html", "/swagger-ui/**", "/api-docs/**").permitAll()
+                .requestMatchers("/subscribe/**","/everlearning/**","https://light-house-web.vercel.app/","https://https://light-house-web.vercel.app/posts/find/list/all","/posts/find/**","/","/auth/**","/ExtraLogin/**","/swagger-ui.html", "/swagger-ui/**", "/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.POST,"/examples/**").hasAuthority("ROLE_TEACHER")
                 .requestMatchers(HttpMethod.DELETE,"/examples/**").hasAuthority("ROLE_TEACHER")
                 .requestMatchers(HttpMethod.PATCH,"/examples/**").hasAuthority("ROLE_TEACHER")
@@ -82,7 +82,7 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://light-house-web-pt.vercel.app");
+                registry.addMapping("/**").allowedOrigins("https://light-house-web.vercel.app/");
             }
         };
     }
@@ -90,7 +90,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
     //setAllowedOriginPatterns 여기에는 값이 "*"이면 안됨
-        configuration.setAllowedOrigins(List.of("https://light-house-web-pt.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://light-house-web.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
